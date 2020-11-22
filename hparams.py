b0a8bdc0e978e201cf7620231deb0a8c8bb14edc
Emilio Morales <mil.mor.mor@gmail.com>
2020-11-21 21:57:27 -0600

    kernel initializer in hparams

--- hparams.py
+++ hparams.py
@@ -1,9 +1,10 @@
 hparams = {'input_size': (256, 256, 3),
            'batch_size': 4,
-           'content_weight': 1e-3,#1e-1
-           'style_weight': 2e-7, #2e-1
-           'learning_rate': 0.0001,
-           'test_size': (1080, 1080, 3),
+           'content_weight': 1e-5,#1e-1
+           'style_weight': 5e-9, #5e-6
+           'learning_rate': 0.001,
+           'test_size': (1280, 1920, 3),
 		   'residual_filters': 64,
-           'residual_layers': 5
+           'residual_layers': 5,
+           'initializer': "glorot_normal",
 }
--- model.py
+++ model.py
@@ -9,27 +9,27 @@ def ImageTransformNet(input_shape=(256, 256, 3)):
 
     x = layers.ZeroPadding2D(padding=2)(inputs)
     x = layers.Conv2D(32, 9, strides=1, padding='same',
-                        kernel_initializer='he_normal')(x)
+                        kernel_initializer=hparams["initializer"])(x)
     x = tfa.layers.InstanceNormalization(axis=3, center=True, 
                                          scale=True,
-                                         beta_initializer="he_normal",
-                                         gamma_initializer="he_normal")(x)
+                                         beta_initializer=hparams["initializer"],
+                                         gamma_initializer=hparams["initializer"])(x)
     x = layers.Activation("relu")(x)
     
     x = layers.Conv2D(64, 3, strides=2,
-                      kernel_initializer='he_normal')(x)
+                      kernel_initializer=hparams["initializer"])(x)
     x = tfa.layers.InstanceNormalization(axis=3, center=True, 
                                          scale=True,
-                                         beta_initializer="he_normal",
-                                         gamma_initializer="he_normal")(x)
+                                         beta_initializer=hparams["initializer"],
+                                         gamma_initializer=hparams["initializer"])(x)
     x = layers.Activation("relu")(x)
     
     x = layers.Conv2D(hparams['residual_filters'], 3, strides=2,
-                      kernel_initializer='he_normal')(x)
+                      kernel_initializer=hparams["initializer"])(x)
     x = tfa.layers.InstanceNormalization(axis=3, center=True, 
                                          scale=True,
-                                         beta_initializer="he_normal",
-                                         gamma_initializer="he_normal")(x)
+                                         beta_initializer=hparams["initializer"],
+                                         gamma_initializer=hparams["initializer"])(x)
     x = layers.Activation("relu")(x)
 
     for size in [hparams['residual_filters']]*hparams['residual_layers']:
@@ -43,33 +43,33 @@ def ImageTransformNet(input_shape=(256, 256, 3)):
 
         x = layers.Activation("relu")(x)
         x = x = layers.Conv2D(size, 3, padding='same',
-                              kernel_initializer='he_normal')(x)
+                              kernel_initializer=hparams["initializer"])(x)
         x = tfa.layers.InstanceNormalization(axis=3, center=True, 
                                              scale=True,
-                                             beta_initializer="he_normal",
-                                             gamma_initializer="he_normal")(x)
+                                             beta_initializer=hparams["initializer"],
+                                             gamma_initializer=hparams["initializer"])(x)
         x = layers.add([x, residual])  # Add back residual
 
     x = layers.UpSampling2D(2)(x)
     x = layers.Conv2D(64, 3, strides=1, padding='same',
-                             kernel_initializer='he_normal')(x)
+                             kernel_initializer=hparams["initializer"])(x)
     x = tfa.layers.InstanceNormalization(axis=3, center=True, 
                                          scale=True,
-                                         beta_initializer="he_normal",
-                                         gamma_initializer="he_normal")(x)
+                                         beta_initializer=hparams["initializer"],
+                                         gamma_initializer=hparams["initializer"])(x)
     x = layers.Activation("relu")(x)   
 
     x = layers.UpSampling2D(2)(x)
     x = layers.Conv2D(32, 3, strides=1, padding='same',
-                      kernel_initializer='he_normal')(x)
+                      kernel_initializer=hparams["initializer"])(x)
     x = tfa.layers.InstanceNormalization(axis=3, center=True, 
                                          scale=True,
-                                         beta_initializer="he_normal",
-                                         gamma_initializer="he_normal")(x)
+                                         beta_initializer=hparams["initializer"],
+                                         gamma_initializer=hparams["initializer"])(x)
     x = layers.Activation("relu")(x)
     
     outputs = layers.Conv2D(3, 9, strides=1, padding='same', dtype='float32',
-                            kernel_initializer='he_normal')(x)
+                            kernel_initializer=hparams["initializer"])(x)
 
     return tf.keras.Model(inputs, outputs)
 
